26550fa57f206982d06b1bf3a243ce874dc71a74
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-19 10:58:32 +0200

    wip

--- src/state/actionTypes.js
+++ src/state/actionTypes.js
@@ -3,3 +3,4 @@ export const DELETE_QUOTE = 'DELETE_QUOTE';
 export const MAKE_QUOTE_OF_THE_DAY = 'MAKE_QUOTE_OF_THE_DAY';
 export const ADD_QUOTE = 'ADD_QUOTE';
 export const MARK_APOCRYPHAL = 'MARK_APOCRYPHAL';
+export const ADD_QUOTES = 'ADD_QUOTES';
